92e3ddaba50c80904f183211c1fc705530298d94
Aayush Trivedi <aayush.t200@gmail.com>
2026-03-27 15:06:03 +0530

    find,includes,insert done

--- src/index.js
+++ src/index.js
@@ -26,4 +26,32 @@ class Tree {
         rootNode.right = this.buildTree(dataArr.slice(middleIndex + 1));
         return rootNode;
     }
+    includes(value) {
+        return this._includes(this.root, value);
+    }
+    _includes(node, value) {
+        if (node === null) return false;
+        if (node.data === value) return true;
+        if (node.data > value) return this._includes(node.left, value);
+        return this._includes(node.right, value);
+    }
+    insert(value) {
+        this.root = this._insert(this.root, value);
+    }
+    _insert(node, value) {
+        if (node === null) return new Node(value);
+        if (node.data === value) return node;
+        if (value < node.data) node.left = this._insert(node.left, value);
+        if (value > node.data) node.right = this._insert(node.right, value);
+        return node;
+    }
+    find(value) {
+        return this._find(this.root, value);
+    }
+    _find(node, value) {
+        if (node === null) return null;
+        if (node.data === value) return node;
+        if (node.data > value) return this._find(node.left, value);
+        return this._find(node.right, value);
+    }
 }
