2e147aafcb8903dc11429a2c7cf0f867603ce357
Aayush Trivedi <aayush.t200@gmail.com>
2026-03-27 11:13:40 +0530

    buildTree done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import "./style.css";
+import './style.css';
+class Node {
+    constructor(data, left, right) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+class Tree {
+    constructor(dataArr) {
+        dataArr = this.cleanArr(dataArr);
+        this.root = this.buildTree(dataArr);
+    }
+    cleanArr(dataArr) {
+        dataArr = [...new Set(dataArr)];
+        dataArr = dataArr.sort(function (a, b) {
+            return a - b;
+        });
+        return dataArr;
+    }
+    buildTree(dataArr) {
+        if (dataArr.length <= 0) return null;
+        let middleIndex = Math.floor(dataArr.length / 2);
+        let rootNode = new Node(dataArr[middleIndex]);
+        rootNode.left = this.buildTree(dataArr.slice(0, middleIndex));
+        rootNode.right = this.buildTree(dataArr.slice(middleIndex + 1));
+        return rootNode;
+    }
+}
