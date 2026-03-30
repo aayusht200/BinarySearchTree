17181e6114273156ea28bab90d35dd9abf11e582
Aayush Trivedi <aayush.t200@gmail.com>
2026-03-30 16:10:25 +0530

    basic versions done

--- src/index.js
+++ src/index.js
@@ -87,16 +87,113 @@ class Tree {
         }
         return node;
     }
-    levelOrderForEach(node) {
-        if (node === null) return null;
-        let tempQue = [node];
+    levelOrder() {
+        if (this.root === null) return [];
         let result = [];
-        while (tempQue.length) {
-            let tempLen = tempQue.length;
-            result.push(tempQue);
-            while (tempLen--) {
-                
-            }
+        let dataQue = [this.root];
+        while (dataQue.length > 0) {
+            let current = dataQue[0];
+            result.push(current.data);
+            if (current.left) dataQue.push(current.left);
+            if (current.right) dataQue.push(current.right);
+            dataQue.shift();
+        }
+        return result;
+    }
+    levelOrderForEach(callback) {
+        if (this.root === null) return;
+        let queue = [this.root];
+        while (queue.length > 0) {
+            let current = queue.shift();
+            callback(current.data);
+            if (current.left) queue.push(current.left);
+            if (current.right) queue.push(current.right);
         }
     }
+    inOrder() {
+        let result = [];
+        this._inOrder(this.root, result);
+        return result;
+    }
+    _inOrder(node, result) {
+        if (node == null) return;
+        this._inOrder(node.left, result);
+        result.push(node.data);
+        this._inOrder(node.right, result);
+    }
+    preOrder() {
+        let result = [];
+        this._preOrder(this.root, result);
+        return result;
+    }
+
+    _preOrder(node, result) {
+        if (node === null) return;
+
+        result.push(node.data);
+        this._preOrder(node.left, result);
+        this._preOrder(node.right, result);
+    }
+
+    postOrder() {
+        let result = [];
+        this._postOrder(this.root, result);
+        return result;
+    }
+
+    _postOrder(node, result) {
+        if (node === null) return;
+        this._postOrder(node.left, result);
+        this._postOrder(node.right, result);
+        result.push(node.data);
+    }
+    height(node = this.root) {
+        if (node === null) return -1;
+        return Math.max(this.height(node.left), this.height(node.right)) + 1;
+    }
+    depth(value) {
+        return this._depth(this.root, value);
+    }
+    _depth(node, value) {
+        if (node === null) return -1;
+        if (node.data === value) return 0;
+        if (value < node.data) {
+            let result = this._depth(node.left, value);
+            if (result === -1) return -1;
+            return result + 1;
+        }
+        if (value > node.data) {
+            let result = this._depth(node.right, value);
+            if (result === -1) return -1;
+            return result + 1;
+        }
+    }
+    isBalanced(node = this.root) {
+        if (!node) return true;
+        let leftHeight = this.height(node.left);
+        let rightHeight = this.height(node.right);
+        if (Math.abs(leftHeight - rightHeight) > 1) return false;
+        return this.isBalanced(node.left) && this.isBalanced(node.right);
+    }
+    reBalance() {
+        let arr = this.inOrder();
+        this.root = this.buildTree(arr);
+    }
 }
+//[2, 5, 7, 10, 15, 12, 20, 1, 21]
+let arr = [1, 2, 3, 4, 5];
+let temp = new Tree(arr);
+console.log(temp.levelOrder());
+console.log(temp.inOrder());
+console.log(temp.preOrder());
+console.log(temp.postOrder());
+console.log(temp.height());
+console.log(temp.depth(1));
+console.log(temp.isBalanced());
+temp.delete(4);
+temp.delete(5);
+console.log(temp.height());
+console.log(temp.isBalanced());
+temp.reBalance();
+console.log(temp.inOrder());
+console.log(temp.isBalanced());
